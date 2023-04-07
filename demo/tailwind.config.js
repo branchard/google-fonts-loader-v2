a5a54f8478db298d7611b3c50ce43fc14e4ea150
branchard <benoit.branchard@gmail.com>
2023-04-07 23:22:01 +0200

    doc: tailwind added to demo

---
+++ demo/tailwind.config.js
@@ -0,0 +1,29 @@
+const plugin = require('tailwindcss/plugin');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./index.html",
+	],
+	theme: {
+		extend: {
+			textShadow: {
+				sm: '0 1px 2px var(--tw-shadow-color)',
+				DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+				lg: '0 8px 16px var(--tw-shadow-color)',
+			},
+		},
+	},
+	plugins: [
+		plugin(function ({matchUtilities, theme}) {
+			matchUtilities(
+				{
+					'text-shadow': (value) => ({
+						textShadow: value,
+					}),
+				},
+				{values: theme('textShadow')}
+			)
+		}),
+	],
+};
